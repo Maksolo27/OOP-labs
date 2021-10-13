54373e459b7550229d36228783320086de786eac
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-10-13 10:25:11 +0300

    Add files via upload

---
+++ Lab2/colors.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "pch.h"
+
+static COLORREF red = RGB(255, 0, 0);
+static COLORREF white = RGB(255, 255, 255);
+static COLORREF black = RGB(0, 0, 0);
+static COLORREF blue = RGB(128, 166, 255);
